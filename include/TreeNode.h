792ae514580ffa81883837447eb5d9085b8f23a1
Barbato Federico <45495935+Developer310301@users.noreply.github.com>
2023-07-21 16:13:39 +0200

    docs: add TreeNode documentations

--- include/TreeNode.h
+++ include/TreeNode.h
@@ -2,7 +2,10 @@
 #define TREENODE_H
 
 namespace treeforest{
-
+    /**
+     * Class that represents a node of a tree
+     * @typedef T type of the content of the node
+    */
     template<typename T>
     class TreeNode{
         private:
@@ -12,16 +15,80 @@ namespace treeforest{
             TreeNode<T>* parent_node;
 
         public:
+            TreeNode() = delete; //remove default constructor
+            /**
+             * Constructor that creates an empty node
+             * @param children_number max number of children that the node can handle
+             * @param node_content pointer to the content of the current node (default nullptr)
+             * @param parent_node pointer to the parent node (default nullptr)
+            */
             TreeNode(const unsigned long children_number, T* node_content=nullptr, T* parent_node=nullptr) : children_size(children_number), children(new TreeNode<T>[children_number]), content(node_content), parent_node(parent_node){}
-            void setContent(T* node_content);
+            
+            /**
+             * Method that set the content of the node
+             * @param node_content pointer to the content of the node
+            */
+            void setContent(T* const node_content);
+            /**
+             * Method that return the content of the node
+             * @return Content of the node
+            */
             T* getContent();
-            void setParentNode(TreeNode<T>* node=nullptr);
-            void deleteParentNode();
-            int setChildrenNode(int pos, TreeNode<T>* node=nullptr);
-            int deleteChildrenNode(int pos);
+
+            /**
+             * Method that set the reference to the parent node
+             * @param node pointer to the parent node
+            */
+            void setParentNode(TreeNode<T>* const node=nullptr);
+
+            /**
+             * Method that returns the reference to the parent node
+             * @return reference to the parent node
+            */
+            T* unsetParentNode();
+
+            /**
+             * Method that try to insert a node into a specific position
+             * @param pos position in which the object will be inserted
+             * @param node node to insert as children
+             * @param force_replace if true, the node to insert replace eventually existing node (default to false)
+             * @return -1 if the position passed is invalid
+             * @return 0 if the object is correctly inserted
+             * @return 1 if the given position is already occupied
+            */
+            int setChildrenNode(const int pos, TreeNode<T>* const node, bool force_replace=false);
+            
+            /**
+             * Method that unset the node in the specific position
+             * @param pos position in which there is the node to retrieve
+             * @return the node in the giving position, nullptr is the position is invalid or the node itself is nullptr
+            */
+            T* unsetChildrenNode(const int pos);
+
+            /**
+             * Operator overload used to compare two nodes
+             * @param node node to compare
+             * @return true if the content of two nodes are equal, false otherwise
+            */
             bool operator==(TreeNode<T> const node) const;
+
+            /**
+             * Operator overload used to compare two nodes
+             * @param node node to compare
+             * @return true if the current node has a content bigger than the passed node, false otherwise
+            */
             bool operator>(TreeNode<T> const node) const;
+            
+            /**
+             * Operator overload used to compare two nodes
+             * @param node node to compare
+             * @return true if the current node has a content smaller than the passed node, false otherwise
+            */
             bool operator<(TreeNode<T> const node) const;
+            
+            /**
+             * Destructor of the class that destroy the content and set all pointers to nullptr
+            */
             ~TreeNode(){
                 parent_node=nullptr;
                 for(int i=0;i<this->children_size;i++){
